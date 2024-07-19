0d23c65bd0ff4f979421718ed742627d2bf32713
Juhász István <juhasz.pisti7253@gmail.com>
2024-07-19 18:29:47 +0200

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by István Juhász on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
